3da68d5ad20511be7f191af4aaad5cff2fd8eeb0
Jon Davidson <jdavidson13@hawkmail.hccfl.edu>
2014-01-29 19:06:33 -0500

    Initial project creation, with a largely blank template.

---
+++ SearchEngine/src/searchengine/SearchEngine.java
@@ -0,0 +1,26 @@
+/*
+ * Project:         Java II (COP 2805) Search Engine
+ *
+ * Authors:         Jon Davidson
+ *                  [          ]
+ *                  [          ]
+ *                  [          ]
+ *
+ * Date Started:    January 29th, 2014
+ */
+
+package searchengine;
+
+/**
+ * @author Jon Davidson
+ */
+public class SearchEngine {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO: code application logic here
+    }
+
+}
