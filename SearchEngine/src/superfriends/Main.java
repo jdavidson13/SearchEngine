a5ac7cad4a150284b027922dab411fcc0564026e
jdavidson13 <jdavidson13@DTEC427-R3-03.academic.hccfl.edu>
2014-02-13 19:37:21 -0500

    Changed package to "superfiriends" "SearchEngine.java" to "Main.java"

---
+++ SearchEngine/src/superfriends/Main.java
@@ -0,0 +1,30 @@
+/*
+ * Project:         Java II (COP 2805) Search Engine
+ * Team:            Super Friends
+ * Authors:         Jon Davidson
+ *                  Thanh J. Duong A.K.A. Chino Caliente
+ *                  John McLain
+ *                  Shaz Hosein        
+ *
+ * Date Started:    January 29th, 2014
+ */
+
+package superfriends;
+
+import java.awt.*;
+/**
+ * @author Jon Davidson
+ * @author Chino Caliente
+ * @author John McLain
+ * @author Shaz Hosein       ]
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
