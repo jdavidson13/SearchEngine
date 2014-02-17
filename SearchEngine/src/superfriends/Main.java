8c696e87d26b0970d90ca214ae2f4c6fccf2c416
hellojoshua1986 <tjoshuaduong@gmail.com>
2014-02-16 20:45:22 -0800

    Update Main.java
    need help

--- SearchEngine/src/superfriends/Main.java
+++ SearchEngine/src/superfriends/Main.java
@@ -12,19 +12,9 @@
 package superfriends;
 
 import java.awt.*;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JRadioButton;
-import javax.swing.JTextField;
-import javax.swing.SwingUtilities;
-import javax.swing.border.Border;
-import javax.xml.stream.Location;
+
+import javax.swing.*;
+
 /**
  * @author Jon Davidson
  * @author Chino Caliente
@@ -37,65 +27,156 @@ public class Main {
      * @param args the command line arguments
      */
     
-    public void createPlayground() {
-        JFrame frame = new JFrame("ForFun Maze");
-        frame.setResizable(false);
-        frame.setLayout(new BorderLayout());
-        Border outline = BorderFactory.createLineBorder(Color.black);
+   public Main() {
+	
+		JFrame f= new JFrame("Search Engine");
+	
+		f.setSize(new Dimension(700, 650));
+		
+		f.setLocationRelativeTo(null);
+		
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		f.setLayout(new BorderLayout());
+		
+		//Top panel
+		
+		JPanel top = new JPanel();
+		
+		top.setLayout(new BorderLayout());
+		
+		top.setBackground(Color.green);
+		
+		top.setPreferredSize(new Dimension(700, 200));
+		
+		//SubPanel top
+		JPanel tP = new JPanel();
+		
+		tP.setLayout(new BorderLayout());
+		
+		tP.setPreferredSize(new Dimension(550, 50));
+		
+		JLabel search = new JLabel("Search Engine");
+		
+		search.setFont(new Font("Serif", Font.BOLD, 18));
+		
+		tP.add(search, BorderLayout.NORTH);
+		
+		top.add(tP, BorderLayout.NORTH);
+		
+		//SubPanel for Search Bar
+		JPanel tPanel = new JPanel();
+		
+		tPanel.setLayout(new BorderLayout());
+		
+		JTextField sInput = new JTextField("Enter Text");
+		
+		sInput.setPreferredSize(new Dimension(40, 40));
+		
+		tPanel.add(sInput, BorderLayout.CENTER);
+		
+		top.add(tPanel, BorderLayout.CENTER);
+		
+		//Sub panel for radioButtons
+		JPanel radioPanel = new JPanel (new FlowLayout (FlowLayout.CENTER));
 
-        Location[][] array = null;
-        JPanel centerPanel = new JPanel();
-        centerPanel.setPreferredSize(new Dimension(700, 400));
-        centerPanel.setMinimumSize(new Dimension(700, 400));
-        centerPanel.setBackground(Color.blue);
-        
-        JPanel northPanel = new JPanel();   
-        northPanel.setPreferredSize(new Dimension(700, 150));
-        northPanel.setBorder(outline);
-        
-        JLabel s = new JLabel("Search Terms: ");
-        JButton search = new JButton("Search");
-        JRadioButton jB1   = new JRadioButton("All of the Search Terms");
-        JRadioButton jB2   = new JRadioButton("Any of the Search Terms");
-        JRadioButton jB3   = new JRadioButton("Exact Phrase");
-        JTextField inputText = new JTextField("In put here");
-        
-        northPanel.add(s);
-        northPanel.add(search);
-        northPanel.add(inputText);
-        northPanel.add(jB3);
-        northPanel.add(jB2);
-        northPanel.add(jB1);
-        
-        JPanel bottomPanel = new JPanel();
-        bottomPanel.setPreferredSize(new Dimension(700, 30));
-        bottomPanel.setBackground(Color.GREEN);
-        bottomPanel.setBorder(outline);
-        bottomPanel.setLayout(new BorderLayout());
-        JButton about = new JButton("About");
-        JButton main = new JButton("Maintenance");
-        bottomPanel.add(main, BorderLayout.WEST);
-        bottomPanel.add(about, BorderLayout.EAST);
-    
-        frame.add(northPanel, BorderLayout.NORTH);
-        frame.add(centerPanel, BorderLayout.CENTER);
-        frame.add(bottomPanel, BorderLayout.SOUTH);
+        JRadioButton button1 = new JRadioButton ("Button 1");
+        JRadioButton button2 = new JRadioButton ("Button 2");
+        JRadioButton button3 = new JRadioButton ("Button 3");
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+        ButtonGroup group = new ButtonGroup ();
+        group.add (button1);
+        group.add (button2);
+		
+        radioPanel.add (button1);
+        radioPanel.add (button2);
+        radioPanel.add (button3);
         
-    }     
-     
-    public static void main(String[] args) {
-         SwingUtilities.invokeLater(new Runnable() {
+        radioPanel.setPreferredSize(new Dimension(100, 50));
+        
+        top.add (radioPanel, BorderLayout.SOUTH);
+        
+        //Right sub Panel
+        JPanel rP = new JPanel();
+        
+        JButton s = new JButton("Search");
+        
+        rP.setBackground(Color.black);
+        
+        rP.setPreferredSize(new Dimension(300, 300));
+        
+        rP.add(s);
+        
+        top.add(rP, BorderLayout.EAST);
+        
+        //Left SubPanel
+        JPanel lP = new JPanel();
+        
+        lP.setBackground(Color.yellow);
+        
+        lP.setPreferredSize(new Dimension (50, 50));
+        
+        JLabel s2 = new JLabel("Search Engine");
+		
+		s2.setFont(new Font("Serif", Font.PLAIN, 14));
+        
+		lP.add(s2);
+		
+		top.add(lP, BorderLayout.WEST);
+        
+		//Center Panel
+		
+		JPanel center = new JPanel();
+		
+		JTextField input = new JTextField("Results", 50);
+		
+		input.setPreferredSize(new Dimension (700, 500));
+		
+		center.add(input);
+		
+		//Bottom Panel
+			
+		JPanel bottom = new JPanel();
+		
+		bottom.setLayout(new FlowLayout());
+		
+		JButton main = new JButton("Maintenance");
+		
+		JButton about = new JButton("About");
+		
+		bottom.setBackground(Color.red);
+		
+		bottom.add(main);
+		
+		bottom.add(about);
+		
+		//Add Panels
+		
+		f.add(center, BorderLayout.CENTER);
+		
+		f.add(top, BorderLayout.NORTH);
+		
+		f.add(bottom, BorderLayout.SOUTH);
+		
+		f.pack();
+		
+		f.setVisible(true);
+		
+	}
+	
+	public static void main(String[] args) {
+				
+		
+		
+        SwingUtilities.invokeLater(new Runnable() {
 
-            @Override
+            @Override    
             public void run() {
-                new Main().createPlayground();
+            
+            	Main form = new Main();
             }
         });
-    }
+
+	}
 
 }
