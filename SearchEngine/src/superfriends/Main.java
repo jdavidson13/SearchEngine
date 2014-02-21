de3282c9e5fc151639372e1a2a496a10a19a5c66
Jon Davidson <jdavidson13@hawkmail.hccfl.edu>
2014-02-20 19:33:56 -0500

    Formatting

--- SearchEngine/src/superfriends/Main.java
+++ SearchEngine/src/superfriends/Main.java
@@ -26,21 +26,41 @@ import javax.swing.JTextField;
  * @author Jon Davidson
  * @author Chino Caliente
  * @author John McLain
- * @author Shaz Hosein       ]
+ * @author Shaz Hosein
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    
-   public static void addComponentsToPane(Container pane) {
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndInvokeGUI();
+            }
+        });
+    }
+
+    private static void createAndInvokeGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Search Engine");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        //Set up the content pane.
+        addComponentsToPane(frame.getContentPane());
+ 
+        //Size and display the window.
+        Insets insets = frame.getInsets();
+        frame.setSize(700, 650);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+ 
+    public static void addComponentsToPane(Container pane) {
         pane.setLayout(null);
  
         JRadioButton rB1 = new JRadioButton("All Search Terms");
         JRadioButton rB2 = new JRadioButton("Any of the Terms");
         JRadioButton rB3 = new JRadioButton("Exact Phrase");
- 
         
         JButton b1 = new JButton("About");
         JButton b2 = new JButton("Maintenance");
@@ -71,59 +91,20 @@ public class Main {
         Dimension size = rB1.getPreferredSize();
         
         jL.setBounds(250 + insets.left, 5 + insets.top, size.width, size.height);
-        
         jLS.setBounds(125 + insets.left, 45 + insets.top, size.width, size.height);
-        
         jlM.setBounds(55 + insets.left, 145 + insets.top, size.width, size.height);
-        
         jF.setBounds(225 + insets.left, 45 + insets.top, size.width + 100, size.height);
-       
         jFR.setBounds(65 + insets.left, 175 + insets.top, size.width + 400, size.height + 300);
         
-        //JBUTTONS
-       
+        // buttons
         b1.setBounds(525 + insets.left, 550 + insets.top, size.width, size.height);
         b2.setBounds(65 + insets.left, 550 + insets.top, size.width, size.height);
         b3.setBounds(500 + insets.left, 45 + insets.top, size.width, size.height);
         
         // radio buttons
-        
         rB1.setBounds(125 + insets.left, 70 + insets.top, size.width, size.height);
-                
         rB2.setBounds(250 + insets.left, 70 + insets.top, size.width, size.height);
-        
         rB3.setBounds(375 + insets.left, 70 + insets.top, size.width, size.height);
-        
-        
-        
     }
  
-    /**
-     * Create the GUI and show it.
-     */
-    private static void createAndInvokeGUI() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("Search Engine");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
- 
-        //Set up the content pane.
-        addComponentsToPane(frame.getContentPane());
- 
-        //Size and display the window.
-        Insets insets = frame.getInsets();
-        frame.setSize(700, 650);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-    }
- 
-    public static void main(String[] args) {
-        //Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndInvokeGUI();
-            }
-        });
-    }
-
 }
