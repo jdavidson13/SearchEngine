685d4596ade58110a856f55e1aebd3ac25581e9e
jon <jon@Red>
2014-02-24 14:15:09 -0500

    Simplified some of Netbeans' auto-generated code, and set look and feel to native.

--- SearchEngine/src/superfriends/Main.java
+++ SearchEngine/src/superfriends/Main.java
@@ -11,6 +11,10 @@
 
 package superfriends;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.*;
+
 /**
  * @author Jon Davidson
  * @author Chino Caliente
@@ -174,35 +178,19 @@ public class Main extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+        
+        // Applies the native system's look-and-feel to the UI
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (   ClassNotFoundException | 
+                    InstantiationException | 
+                    IllegalAccessException | 
+                    UnsupportedLookAndFeelException ex) {
+            System.out.println("Unable to load native look and feel");
         }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Main().setVisible(true);
-            }
-        });
+        
+        // Instantiates and shows the main JFrame
+        new Main().setVisible(true);
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
